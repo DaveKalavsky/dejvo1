68ba0f0ad362b370bfc4f253140d06d29e69ef44
DaveKalavsky <55738228+DaveKalavsky@users.noreply.github.com>
2019-10-03 18:14:55 +0200

    verzia 5

--- app/src/main/java/com/example/myapplication/RegistrationActivity.java
+++ app/src/main/java/com/example/myapplication/RegistrationActivity.java
@@ -1,7 +1,9 @@
  package com.example.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -9,6 +11,12 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
  public class RegistrationActivity extends AppCompatActivity {
 
@@ -17,11 +25,23 @@ import android.widget.TextView;
     private Button btnReg;
     private TextView mSignin;
 
+    private ProgressDialog mDialog;
+
+
+    //firebase..
+
+        private FirebaseAuth mAuth;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+
+        mAuth=FirebaseAuth.getInstance();
+
+        mDialog=new ProgressDialog(this);
 
         registration();
 
@@ -48,6 +68,25 @@ import android.widget.TextView;
                 if (TextUtils.isEmpty(pass))  {
                     mPass.setError("Password is Required.");
                 }
+
+                mDialog.setMessage("Processing..");
+
+
+               mAuth.createUserWithEmailAndPassword(email,pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+                        if(task.isSuccessful()) {
+
+                            Toast.makeText(getApplicationContext(),"Registration complete",Toast.LENGTH_SHORT).show();
+
+                        }else {
+                            mDialog.dismiss();
+                            Toast.makeText(getApplicationContext(),"Registration failed",Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+                });
 
 
 
