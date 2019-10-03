d8e02af8bfd4e2110a96691a3b2dde3d9a0ed68a
DaveKalavsky <55738228+DaveKalavsky@users.noreply.github.com>
2019-10-03 12:07:17 +0200

    verzia 4

--- app/src/main/java/com/example/myapplication/RegistrationActivity.java
+++ app/src/main/java/com/example/myapplication/RegistrationActivity.java
@@ -2,6 +2,7 @@
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
@@ -54,6 +55,12 @@ import android.widget.TextView;
             }
         });
 
+        mSignin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(),MainActivity.class));
+            }
+        });
 
 
     }
