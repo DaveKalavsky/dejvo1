66f6ca90e9ba77ae39eb8901a1592b5901e0425c
DaveKalavsky <55738228+DaveKalavsky@users.noreply.github.com>
2019-11-18 22:21:26 +0100

    verzia 9

--- app/src/main/java/com/example/myapplication/HomeActivity.java
+++ app/src/main/java/com/example/myapplication/HomeActivity.java
@@ -1,14 +1,31 @@
 package com.example.myapplication;
 
+import android.os.Bundle;
+import android.view.MenuItem;
+import androidx.appcompat.widget.Toolbar;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.google.android.material.navigation.NavigationView;
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        Toolbar toolbar=findViewById(R.id.my_toolbar);
+        toolbar.setTitle("Expense Manager");
+        setSupportActionBar(toolbar);
+
     }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        return false;
+    }
+
 }
