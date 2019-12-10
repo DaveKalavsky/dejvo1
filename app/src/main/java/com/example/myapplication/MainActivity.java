10200e95cbc3b59eea7960b27e77bfba641bd90b
DaveKalavsky <55738228+DaveKalavsky@users.noreply.github.com>
2019-12-10 22:50:21 +0100

    verzia 11

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -9,6 +10,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,11 +21,24 @@ public class MainActivity extends AppCompatActivity {
     private TextView mForgetPassword;
     private TextView mSignupHere;
 
+    private ProgressDialog mDialog;
+    private FirebaseAuth mAuth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mAuth=FirebaseAuth.getInstance();
+
+        if (mAuth.getCurrentUser()!=null) {
+
+            startActivity(new Intent(getApplicationContext(),HomeActivity.class));
+
+        }
+
+        mDialog=new ProgressDialog(this);
 
         loginDetails();
 
